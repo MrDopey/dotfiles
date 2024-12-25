937d16d13170beb209068c45c5f1c8b14f98ffc2
Mr Dopey <beforewards@gmail.com>
2024-12-25 21:00:23 +1100

    feat(secret): added devcontainer variables secret.sh

--- untracked/secrets.template.sh
+++ untracked/secrets.template.sh
@@ -1,5 +1,9 @@
 # These are injected into the .zshrc file
 export ZOOM_TOKEN=
 
+export DEV_CONTAINER_USER_ID=$(id -u)
+export DEV_CONTAINER_GROUP_ID=$(id -g)
+
+
 # export LOGSEQ_DEV_NOTES_DIR=/mnt/c/Sandbox/
 # export LOGSEQ_DEV_NOTES_DIR=~/sandbox/
