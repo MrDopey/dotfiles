569f8ed638b1a8097717dcfc4f218c8811a37e56
Mr Dopey <beforewards@gmail.com>
2025-07-22 01:27:30 +1000

    nvim(telescope): included option to search through hidden files

--- .config/nvim/lua/telescope-setup.lua
+++ .config/nvim/lua/telescope-setup.lua
@@ -2,6 +2,10 @@
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    file_ignore_patterns = { -- Always ignore
+      "^.git/",
+      "^.venv/",
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -71,7 +75,10 @@ vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]e
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sh', function()
+  require('telescope.builtin').find_files({ hidden = true })
+end, { desc = '[S]earch [h]idden' })
+-- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 -- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
