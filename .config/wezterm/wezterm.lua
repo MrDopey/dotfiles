03eed8aea9655bc26e77cd9adc016a40ac35dafc
Mr Dopey <beforewards@gmail.com>
2025-08-03 01:47:02 +1000

    wezterm: changed themes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,10 +10,10 @@ if wezterm.config_builder then
 end
 
 -- Set the window background opacity to 0.7
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.80
 config.window_close_confirmation = "NeverPrompt"
 config.skip_close_confirmation_for_processes_named = { "bash", "zsh", "fish", "tmux" }
-config.color_scheme = "Monokai Pro (Gogh)"
+config.color_scheme = "Monokai Remastered"
 
 -- Check if the current OS is macOS (Darwin)
 if wezterm.target_triple:find("darwin") then
