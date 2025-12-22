3a4e15328ca9f45c1a849b76e2a024871593f9f9
Mr Dopey <beforewards@gmail.com>
2025-12-23 00:40:24 +1100

    wezterm: added alt + delete to delete word in front

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -57,6 +57,8 @@ elseif wezterm.target_triple:find("linux") then
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 		-- Make Option-Right equivalent to Alt-f; forward-word
 		{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+		-- Make alt + del, delete a word
+		{ key = "Delete", mods = "OPT", action = wezterm.action({ SendString = "\x1bd" }) },
 	}
 end
 
