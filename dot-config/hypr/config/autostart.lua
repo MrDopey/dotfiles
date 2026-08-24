6c8a471d397208caddba3b64353b1d00deff1bfc
Mr Dopey <beforewards@gmail.com>
2026-08-25 00:40:13 +1000

    hypr: added back a background

--- dot-config/hypr/config/autostart.lua
+++ dot-config/hypr/config/autostart.lua
@@ -1,6 +1,7 @@
 -- https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function()
+	hl.exec_cmd("swaybg -o \\* -i /usr/share/wallpapers/cachyos-wallpapers/CachyOS_GreenSpace.png -m fill")
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("fcitx5 -d")
 	hl.exec_cmd("mako")
